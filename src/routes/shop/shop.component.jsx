64d3dcea70660385cae4a6ded1351b05d742a8a5
alaindlctvop <alain_dlc@outlook.com>
2023-03-07 14:41:50 +0100

    added shopping component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,15 @@
 import { useContext } from "react";
-
 import { ProductsContext } from "../../context/products.context";
+import ProductCard from "../../components/product-card/product-card.component";
+
+import './shop.styles.scss';
 
 const Shop = () => {
     const { products } = useContext(ProductsContext);
     return (
-        <div >
-            {products.map(({id,name,imageUrl,price}) => (
-                <div key={id}>
-                    <h1>{name}</h1>
-                    <img src={imageUrl} alt="BigCo Inc. logo" />
-                    <h3>{price}</h3>
-                </div>
+        <div className="products-container">
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
             ))}
         </div>
     )
